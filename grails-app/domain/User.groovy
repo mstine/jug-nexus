eb5be3cafac5faa5561d60963cda3bef62dfba65
mstine <mstine@L090047.local>
2009-08-08 19:16:25 -0500

    add equals and hashCode

--- grails-app/domain/User.groovy
+++ grails-app/domain/User.groovy
@@ -3,11 +3,11 @@
  */
 class User {
   static transients = ['pass']
-  static hasMany = [authorities: Role, eventsAttending:EventAttendeeRegistration, eventsSpeaking:EventSpeakerAssignment]
+  static hasMany = [authorities: Role, eventsAttending: EventAttendeeRegistration, eventsSpeaking: EventSpeakerAssignment]
   static belongsTo = Role
 
   String username
-  /** MD5 Password  */
+  /** MD5 Password   */
   String passwd
   String firstName
   String lastName
@@ -21,25 +21,44 @@ class User {
   boolean moderated = false
   String whyIWantToJoin
 
-  /** plain password to create a MD5 password  */
+  /** plain password to create a MD5 password   */
   String pass = '[secret]'
 
   static constraints = {
     username(blank: false, unique: true)
     passwd(blank: false)
-    firstName(blank:false)
-    lastName(blank:false)
-    location(nullable:true)
-    company(nullable:true)
-    email(blank:false,email:true)
-    bio(nullable:true,maxSize:4000)
-    website(nullable:true,url:true)
-    blogFeed(nullable:true,url:true)
-    twitterNickname(nullable:true)
-    whyIWantToJoin(blank:false,maxSize:4000)
+    firstName(blank: false)
+    lastName(blank: false)
+    location(nullable: true)
+    company(nullable: true)
+    email(blank: false, email: true)
+    bio(nullable: true, maxSize: 4000)
+    website(nullable: true, url: true)
+    blogFeed(nullable: true, url: true)
+    twitterNickname(nullable: true)
+    whyIWantToJoin(blank: false, maxSize: 4000)
   }
 
   String toString() {
     "${firstName} ${lastName}"
+  }
+
+
+
+
+  boolean equals(o) {
+    if (this.is(o)) return true;
+
+    if (!o || getClass() != o.class) return false;
+
+    User user = (User) o;
+
+    if (!username.equals(user.username)) return false;
+
+    return true;
+  }
+
+  int hashCode() {
+    return username.hashCode();
   }
 }
