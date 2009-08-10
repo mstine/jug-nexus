714e933b19e850be659f49be0a86dc36fec31b4d
mstine <mstine@L090047.local>
2009-08-10 16:06:44 -0500

    setup a cached twitter timeline for the front page that refreshes every 5 minutes

---
+++ test/unit/TwitterCacheServiceTests.groovy
@@ -0,0 +1,15 @@
+import grails.test.*
+
+class TwitterCacheServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
