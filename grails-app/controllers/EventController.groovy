3cdf8f4bd95b6e245242306afaaa6685a0cda6f5
mstine <mstine@L100102.local>
2009-08-26 22:05:21 -0500

    "tweet" various application events

--- grails-app/controllers/EventController.groovy
+++ grails-app/controllers/EventController.groovy
@@ -1,99 +1,112 @@
-
-
 class EventController {
-    
-    def index = { redirect(action:list,params:params) }
 
-    // the delete, save and update actions only accept POST requests
-    static allowedMethods = [delete:'POST', save:'POST', update:'POST']
+  def twitterService
+  def tinyurlService
 
-    def list = {
-        params.max = Math.min( params.max ? params.max.toInteger() : 10,  100)
-        [ eventInstanceList: Event.list( params ), eventInstanceTotal: Event.count() ]
+  def index = { redirect(action: list, params: params) }
+
+  // the delete, save and update actions only accept POST requests
+  static allowedMethods = [delete: 'POST', save: 'POST', update: 'POST']
+
+  def list = {
+    params.max = Math.min(params.max ? params.max.toInteger() : 10, 100)
+    [eventInstanceList: Event.list(params), eventInstanceTotal: Event.count()]
+  }
+
+  def show = {
+    def eventInstance = Event.get(params.id)
+
+    if (!eventInstance) {
+      flash.message = "Event not found with id ${params.id}"
+      redirect(action: list)
     }
+    else { return [eventInstance: eventInstance] }
+  }
 
-    def show = {
-        def eventInstance = Event.get( params.id )
-
-        if(!eventInstance) {
-            flash.message = "Event not found with id ${params.id}"
-            redirect(action:list)
-        }
-        else { return [ eventInstance : eventInstance ] }
+  def delete = {
+    def eventInstance = Event.get(params.id)
+    if (eventInstance) {
+      try {
+        eventInstance.delete(flush: true)
+        flash.message = "Event ${params.id} deleted"
+        redirect(action: list)
+      }
+      catch (org.springframework.dao.DataIntegrityViolationException e) {
+        flash.message = "Event ${params.id} could not be deleted"
+        redirect(action: show, id: params.id)
+      }
     }
-
-    def delete = {
-        def eventInstance = Event.get( params.id )
-        if(eventInstance) {
-            try {
-                eventInstance.delete(flush:true)
-                flash.message = "Event ${params.id} deleted"
-                redirect(action:list)
-            }
-            catch(org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "Event ${params.id} could not be deleted"
-                redirect(action:show,id:params.id)
-            }
-        }
-        else {
-            flash.message = "Event not found with id ${params.id}"
-            redirect(action:list)
-        }
+    else {
+      flash.message = "Event not found with id ${params.id}"
+      redirect(action: list)
     }
+  }
 
-    def edit = {
-        def eventInstance = Event.get( params.id )
+  def edit = {
+    def eventInstance = Event.get(params.id)
 
-        if(!eventInstance) {
-            flash.message = "Event not found with id ${params.id}"
-            redirect(action:list)
-        }
-        else {
-            return [ eventInstance : eventInstance ]
-        }
+    if (!eventInstance) {
+      flash.message = "Event not found with id ${params.id}"
+      redirect(action: list)
     }
-
-    def update = {
-        def eventInstance = Event.get( params.id )
-        if(eventInstance) {
-            if(params.version) {
-                def version = params.version.toLong()
-                if(eventInstance.version > version) {
-                    
-                    eventInstance.errors.rejectValue("version", "event.optimistic.locking.failure", "Another user has updated this Event while you were editing.")
-                    render(view:'edit',model:[eventInstance:eventInstance])
-                    return
-                }
-            }
-            eventInstance.properties = params
-            if(!eventInstance.hasErrors() && eventInstance.save()) {
-                flash.message = "Event ${params.id} updated"
-                redirect(action:show,id:eventInstance.id)
-            }
-            else {
-                render(view:'edit',model:[eventInstance:eventInstance])
-            }
-        }
-        else {
-            flash.message = "Event not found with id ${params.id}"
-            redirect(action:list)
-        }
+    else {
+      return [eventInstance: eventInstance]
     }
+  }
 
-    def create = {
-        def eventInstance = new Event()
-        eventInstance.properties = params
-        return ['eventInstance':eventInstance]
-    }
+  def update = {
+    def eventInstance = Event.get(params.id)
+    if (eventInstance) {
+      if (params.version) {
+        def version = params.version.toLong()
+        if (eventInstance.version > version) {
 
-    def save = {
-        def eventInstance = new Event(params)
-        if(eventInstance.save()) {
-            flash.message = "Event ${eventInstance.id} created"
-            redirect(action:show,id:eventInstance.id)
+          eventInstance.errors.rejectValue("version", "event.optimistic.locking.failure", "Another user has updated this Event while you were editing.")
+          render(view: 'edit', model: [eventInstance: eventInstance])
+          return
         }
-        else {
-            render(view:'create',model:[eventInstance:eventInstance])
-        }
+      }
+      eventInstance.properties = params
+      if (!eventInstance.hasErrors() && eventInstance.save()) {
+        flash.message = "Event ${params.id} updated"
+
+        def link = createLink(controller: "event", action: "show", id: eventInstance.id, absolute: true)
+        def tinyLink = tinyurlService.tiny(link)
+        twitterService.setStatus("Updated Event Posted for ${formatDate(format:'MM/dd/yyyy', date:eventInstance.startTime)}: ${eventInstance.title} ${tinyLink}", [username: grailsApplication.config.twitter.username,
+                password: grailsApplication.config.twitter.password])
+
+        redirect(action: show, id: eventInstance.id)
+      }
+      else {
+        render(view: 'edit', model: [eventInstance: eventInstance])
+      }
     }
+    else {
+      flash.message = "Event not found with id ${params.id}"
+      redirect(action: list)
+    }
+  }
+
+  def create = {
+    def eventInstance = new Event()
+    eventInstance.properties = params
+    return ['eventInstance': eventInstance]
+  }
+
+  def save = {
+    def eventInstance = new Event(params)
+    if (eventInstance.save()) {
+      flash.message = "Event ${eventInstance.id} created"
+
+      def link = createLink(controller: "event", action: "show", id: eventInstance.id, absolute: true)
+      def tinyLink = tinyurlService.tiny(link)
+      twitterService.setStatus("New Event Posted for ${formatDate(format:'MM/dd/yyyy', date:eventInstance.startTime)}: ${eventInstance.title} ${tinyLink}", [username: grailsApplication.config.twitter.username,
+              password: grailsApplication.config.twitter.password])
+
+      redirect(action: show, id: eventInstance.id)
+    }
+    else {
+      render(view: 'create', model: [eventInstance: eventInstance])
+    }
+  }
 }
--- grails-app/controllers/EventSpeakerAssignmentController.groovy
+++ grails-app/controllers/EventSpeakerAssignmentController.groovy
@@ -1,110 +1,157 @@
 import com.lucastex.grails.fileuploader.UFile
 
 class EventSpeakerAssignmentController {
-    
-    def index = { redirect(action:list,params:params) }
 
-    // the delete, save and update actions only accept POST requests
-    static allowedMethods = [delete:'POST', save:'POST', update:'POST']
+  def twitterService
+  def tinyurlService
 
-    def list = {
-        params.max = Math.min( params.max ? params.max.toInteger() : 10,  100)
-        [ eventSpeakerAssignmentInstanceList: EventSpeakerAssignment.list( params ), eventSpeakerAssignmentInstanceTotal: EventSpeakerAssignment.count() ]
+  def index = { redirect(action: list, params: params) }
+
+  // the delete, save and update actions only accept POST requests
+  static allowedMethods = [delete: 'POST', save: 'POST', update: 'POST']
+
+  def list = {
+    params.max = Math.min(params.max ? params.max.toInteger() : 10, 100)
+    [eventSpeakerAssignmentInstanceList: EventSpeakerAssignment.list(params), eventSpeakerAssignmentInstanceTotal: EventSpeakerAssignment.count()]
+  }
+
+  def show = {
+    def eventSpeakerAssignmentInstance = EventSpeakerAssignment.get(params.id)
+
+    if (!eventSpeakerAssignmentInstance) {
+      flash.message = "EventSpeakerAssignment not found with id ${params.id}"
+      redirect(action: list)
+    }
+    else { return [eventSpeakerAssignmentInstance: eventSpeakerAssignmentInstance] }
+  }
+
+  def delete = {
+    def eventSpeakerAssignmentInstance = EventSpeakerAssignment.get(params.id)
+    if (eventSpeakerAssignmentInstance) {
+      try {
+        eventSpeakerAssignmentInstance.delete(flush: true)
+        flash.message = "EventSpeakerAssignment ${params.id} deleted"
+        redirect(action: list)
+      }
+      catch (org.springframework.dao.DataIntegrityViolationException e) {
+        flash.message = "EventSpeakerAssignment ${params.id} could not be deleted"
+        redirect(action: show, id: params.id)
+      }
+    }
+    else {
+      flash.message = "EventSpeakerAssignment not found with id ${params.id}"
+      redirect(action: list)
+    }
+  }
+
+  def edit = {
+    def eventSpeakerAssignmentInstance = EventSpeakerAssignment.get(params.id)
+
+    if (!eventSpeakerAssignmentInstance) {
+      flash.message = "EventSpeakerAssignment not found with id ${params.id}"
+      redirect(action: list)
+    }
+    else {
+      return [eventSpeakerAssignmentInstance: eventSpeakerAssignmentInstance]
+    }
+  }
+
+  def update = {
+    def eventSpeakerAssignmentInstance = EventSpeakerAssignment.get(params.id)
+    if (eventSpeakerAssignmentInstance) {
+      if (params.version) {
+        def version = params.version.toLong()
+        if (eventSpeakerAssignmentInstance.version > version) {
+
+          eventSpeakerAssignmentInstance.errors.rejectValue("version", "eventSpeakerAssignment.optimistic.locking.failure", "Another user has updated this EventSpeakerAssignment while you were editing.")
+          render(view: 'edit', model: [eventSpeakerAssignmentInstance: eventSpeakerAssignmentInstance])
+          return
+        }
+      }
+      eventSpeakerAssignmentInstance.properties = params
+      if (!eventSpeakerAssignmentInstance.hasErrors() && eventSpeakerAssignmentInstance.save()) {
+        flash.message = "EventSpeakerAssignment ${params.id} updated"
+
+        def link = createLink(controller: "eventSpeakerAssignment", action: "show", id: eventSpeakerAssignmentInstance.id, absolute: true)
+        def tinyLink = tinyurlService.tiny(link)
+
+        def prefix = "Updated "
+
+        if (eventSpeakerAssignmentInstance.lightningTalk) {
+          prefix += "Lightning Talk"
+        } else {
+          prefix += "Topic"
+        }
+        prefix += " by ${eventSpeakerAssignmentInstance.user.firstName} ${eventSpeakerAssignmentInstance.user.lastName}"
+
+        twitterService.setStatus("${prefix}: ${eventSpeakerAssignmentInstance.topic} ${tinyLink}", [username: grailsApplication.config.twitter.username,
+                password: grailsApplication.config.twitter.password])
+
+        redirect(action: show, id: eventSpeakerAssignmentInstance.id)
+      }
+      else {
+        render(view: 'edit', model: [eventSpeakerAssignmentInstance: eventSpeakerAssignmentInstance])
+      }
+    }
+    else {
+      flash.message = "EventSpeakerAssignment not found with id ${params.id}"
+      redirect(action: list)
+    }
+  }
+
+  def create = {
+    def eventSpeakerAssignmentInstance = new EventSpeakerAssignment()
+    eventSpeakerAssignmentInstance.properties = params
+    return ['eventSpeakerAssignmentInstance': eventSpeakerAssignmentInstance]
+  }
+
+  def save = {
+    def eventSpeakerAssignmentInstance = new EventSpeakerAssignment(params)
+    eventSpeakerAssignmentInstance.merge()
+    if (eventSpeakerAssignmentInstance.save()) {
+      flash.message = "EventSpeakerAssignment ${eventSpeakerAssignmentInstance.id} created"
+
+      def link = createLink(controller: "eventSpeakerAssignment", action: "show", id: eventSpeakerAssignmentInstance.id, absolute: true)
+      def tinyLink = tinyurlService.tiny(link)
+
+      def prefix = "New "
+
+      if (eventSpeakerAssignmentInstance.lightningTalk) {
+        prefix += "Lightning Talk"
+      } else {
+        prefix += "Topic"
+      }
+      prefix += " by ${eventSpeakerAssignmentInstance.user.firstName} ${eventSpeakerAssignmentInstance.user.lastName}"
+
+      twitterService.setStatus("${prefix}: ${eventSpeakerAssignmentInstance.topic} ${tinyLink}", [username: grailsApplication.config.twitter.username,
+              password: grailsApplication.config.twitter.password])
+
+      redirect(action: show, id: eventSpeakerAssignmentInstance.id)
+    }
+    else {
+      render(view: 'create', model: [eventSpeakerAssignmentInstance: eventSpeakerAssignmentInstance])
+    }
+  }
+
+  def addFile = {
+    UFile uploadedFile = UFile.get(params.ufileId)
+    EventSpeakerAssignment eventSpeakerAssignment = EventSpeakerAssignment.get(params.id)
+
+    eventSpeakerAssignment.addToFiles(uploadedFile)
+    eventSpeakerAssignment.save()
+
+    def prefix = "New File Uploaded for ${eventSpeakerAssignment.user.firstName} ${eventSpeakerAssignment.user.lastName}'s "
+    if (eventSpeakerAssignment.lightningTalk) {
+      prefix += "Lightning Talk"
+    } else {
+      prefix += "Topic"
     }
 
-    def show = {
-        def eventSpeakerAssignmentInstance = EventSpeakerAssignment.get( params.id )
+    def link = createLink(controller: "eventSpeakerAssignment", action: "show", id: eventSpeakerAssignment.id, absolute: true)
+    def tinyLink = tinyurlService.tiny(link)
+    twitterService.setStatus("${prefix}: ${eventSpeakerAssignment.topic} ${tinyLink}", [username: grailsApplication.config.twitter.username,
+            password: grailsApplication.config.twitter.password])
 
-        if(!eventSpeakerAssignmentInstance) {
-            flash.message = "EventSpeakerAssignment not found with id ${params.id}"
-            redirect(action:list)
-        }
-        else { return [ eventSpeakerAssignmentInstance : eventSpeakerAssignmentInstance ] }
-    }
-
-    def delete = {
-        def eventSpeakerAssignmentInstance = EventSpeakerAssignment.get( params.id )
-        if(eventSpeakerAssignmentInstance) {
-            try {
-                eventSpeakerAssignmentInstance.delete(flush:true)
-                flash.message = "EventSpeakerAssignment ${params.id} deleted"
-                redirect(action:list)
-            }
-            catch(org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "EventSpeakerAssignment ${params.id} could not be deleted"
-                redirect(action:show,id:params.id)
-            }
-        }
-        else {
-            flash.message = "EventSpeakerAssignment not found with id ${params.id}"
-            redirect(action:list)
-        }
-    }
-
-    def edit = {
-        def eventSpeakerAssignmentInstance = EventSpeakerAssignment.get( params.id )
-
-        if(!eventSpeakerAssignmentInstance) {
-            flash.message = "EventSpeakerAssignment not found with id ${params.id}"
-            redirect(action:list)
-        }
-        else {
-            return [ eventSpeakerAssignmentInstance : eventSpeakerAssignmentInstance ]
-        }
-    }
-
-    def update = {
-        def eventSpeakerAssignmentInstance = EventSpeakerAssignment.get( params.id )
-        if(eventSpeakerAssignmentInstance) {
-            if(params.version) {
-                def version = params.version.toLong()
-                if(eventSpeakerAssignmentInstance.version > version) {
-                    
-                    eventSpeakerAssignmentInstance.errors.rejectValue("version", "eventSpeakerAssignment.optimistic.locking.failure", "Another user has updated this EventSpeakerAssignment while you were editing.")
-                    render(view:'edit',model:[eventSpeakerAssignmentInstance:eventSpeakerAssignmentInstance])
-                    return
-                }
-            }
-            eventSpeakerAssignmentInstance.properties = params
-            if(!eventSpeakerAssignmentInstance.hasErrors() && eventSpeakerAssignmentInstance.save()) {
-                flash.message = "EventSpeakerAssignment ${params.id} updated"
-                redirect(action:show,id:eventSpeakerAssignmentInstance.id)
-            }
-            else {
-                render(view:'edit',model:[eventSpeakerAssignmentInstance:eventSpeakerAssignmentInstance])
-            }
-        }
-        else {
-            flash.message = "EventSpeakerAssignment not found with id ${params.id}"
-            redirect(action:list)
-        }
-    }
-
-    def create = {
-        def eventSpeakerAssignmentInstance = new EventSpeakerAssignment()
-        eventSpeakerAssignmentInstance.properties = params
-        return ['eventSpeakerAssignmentInstance':eventSpeakerAssignmentInstance]
-    }
-
-    def save = {
-        def eventSpeakerAssignmentInstance = new EventSpeakerAssignment(params)
-        eventSpeakerAssignmentInstance.merge()
-        if(eventSpeakerAssignmentInstance.save()) {
-            flash.message = "EventSpeakerAssignment ${eventSpeakerAssignmentInstance.id} created"
-            redirect(action:show,id:eventSpeakerAssignmentInstance.id)
-        }
-        else {
-            render(view:'create',model:[eventSpeakerAssignmentInstance:eventSpeakerAssignmentInstance])
-        }
-    }
-
-    def addFile = {
-      UFile uploadedFile = UFile.get(params.ufileId)
-      EventSpeakerAssignment eventSpeakerAssignment = EventSpeakerAssignment.get(params.id)
-
-      eventSpeakerAssignment.addToFiles(uploadedFile)
-      eventSpeakerAssignment.save()
-
-      redirect action: "show", id: params.id
-    }
+    redirect action: "show", id: params.id
+  }
 }
